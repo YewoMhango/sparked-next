68d0e9f4efe70822178937f63ab9c9abb95adee0
Yewo Mhango <mhangoyewoh@gmail.com>
2024-04-08 15:51:47 +0200

    Configured realm to run in memory

--- src/app/api/lib/db/realm.ts
+++ src/app/api/lib/db/realm.ts
@@ -1,5 +1,39 @@
-import Realm from "realm";
+import Realm from 'realm';
+import {
+  bookmarksSchema,
+  course_topicsSchema,
+  coursesSchema,
+  courses_languagesSchema,
+  feed_backSchema,
+  languagesSchema,
+  preferencesSchema,
+  programsSchema,
+  schoolSchema,
+  schoolsSchema,
+  settingsSchema,
+  user_permissionsSchema,
+  user_rolesSchema,
+} from './schema';
 
 export const realmApp = new Realm.App({
   id: process.env.REALM_APP_ID as string,
+});
+
+export const realm = Realm.open({
+  schema: [
+    bookmarksSchema,
+    course_topicsSchema,
+    coursesSchema,
+    courses_languagesSchema,
+    feed_backSchema,
+    languagesSchema,
+    schoolSchema,
+    schoolsSchema,
+    preferencesSchema,
+    settingsSchema,
+    user_permissionsSchema,
+    user_rolesSchema,
+    programsSchema,
+  ],
+  inMemory: true,
 });
